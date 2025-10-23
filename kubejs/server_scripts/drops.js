0ef5d2b3a6d6e25c0860de147f4eb00bacc0d19f
Pyrolyzed <pyrolyzed@proton.me>
2025-10-23 01:43:18 -0500

    changes

--- kubejs/server_scripts/drops.js
+++ kubejs/server_scripts/drops.js
@@ -1,5 +1,7 @@
 LootJS.lootTables(event => {
-    function modifyDrop(entity, drop, count) {
+    function modifyEntityDrop(mod, entityId, drop, count) {
+        mod = mod ?? "minecraft"
+        const entity = `${mod}:entities/${entityId}`
         event.getLootTable(entity).firstPool().modifyItemEntry(itemEntry => {
             if (itemEntry.item.id === drop) {
                 itemEntry.setCount(count)
@@ -8,13 +10,21 @@ LootJS.lootTables(event => {
         })
     }
 
-    modifyDrop("minecraft:entities/shulker", "minecraft:shulker_shell", 2)
-    modifyDrop("minecraft:entities/enderman", "minecraft:ender_pearl", 1)
-    modifyDrop("minecraft:entities/cow", "minecraft:leather", [1, 3])
-    modifyDrop("minecraft:entities/horse", "minecraft:leather", [1, 3])
-    modifyDrop("minecraft:entities/creeper", "minecraft:gunpowder", [1, 3])
-    modifyDrop("minecraft:entities/zombie", "minecraft:rotten_flesh", [1, 3])
-    modifyDrop("minecraft:entities/skeleton", "minecraft:bone", [1, 3])
-    modifyDrop("minecraft:entities/spider", "minecraft:string", [1, 3])
-    modifyDrop("minecraft:entities/spider", "minecraft:spider_eye", 1)
+    modifyEntityDrop("shulker", "minecraft:shulker_shell", 2)
+    modifyEntityDrop("enderman", "minecraft:ender_pearl", 1)
+    modifyEntityDrop("cow", "minecraft:leather", [1, 3])
+    modifyEntityDrop("horse", "minecraft:leather", [1, 3])
+    modifyEntityDrop("creeper", "minecraft:gunpowder", [1, 3])
+    modifyEntityDrop("zombie", "minecraft:rotten_flesh", [1, 3])
+    modifyEntityDrop("skeleton", "minecraft:bone", [1, 3])
+    modifyEntityDrop("skeleton", "minecraft:arrow", [1, 2])
+    modifyEntityDrop("spider", "minecraft:string", [1, 3])
+    modifyEntityDrop("spider", "minecraft:spider_eye", 1)
+    modifyEntityDrop("blaze", "minecraft:blaze_rod", [1, 2])
+    modifyEntityDrop("ghast", "minecraft:ghast_tear", [1, 2])
+    modifyEntityDrop("magma_cube", "minecraft:magma_cream", [1, 2])
+    modifyEntityDrop("phantom", "minecraft:phantom_membrane", [1, 2])
+    modifyEntityDrop("slime", "minecraft:slime_ball", [1, 3])
+    modifyEntityDrop("wither_skeleton", "minecraft:bone", [1, 3])
+    modifyEntityDrop("warden", "apothic_enchanting:warden_tendril", [2, 3])
 })
