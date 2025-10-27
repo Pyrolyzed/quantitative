198d1c16e45f8ba5feec013a50dffdae8620bbaa
Pyrolyzed <pyrolyzed@proton.me>
2025-10-27 17:36:38 -0500

    Changes :)

--- kubejs/server_scripts/mods/mekanism.js
+++ kubejs/server_scripts/mods/mekanism.js
@@ -1,13 +1,16 @@
 ServerEvents.recipes(event => {
+
+    function removeAllRecipes(itemIds) {
+        itemIds.forEach(itemId => event.remove({output: itemId}))
+    }
+
     // Remove vanilla material Mekanism paxels in favor of Pickle Tweak's paxels
-    event.remove({
-        output: [
-            "mekanismtools:wood_paxel",
-            "mekanismtools:stone_paxel",
-            "mekanismtools:gold_paxel",
-            "mekanismtools:iron_paxel",
-            "mekanismtools:diamond_paxel",
-            "mekanismtools:netherite_paxel"
-        ]
-    })
+    removeAllRecipes([
+        "mekanismtools:wood_paxel",
+        "mekanismtools:stone_paxel",
+        "mekanismtools:gold_paxel",
+        "mekanismtools:iron_paxel",
+        "mekanismtools:diamond_paxel",
+        "mekanismtools:netherite_paxel"
+    ])
 })
